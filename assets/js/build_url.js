eb7d346ca6dbf2a4e2f5fc8ff24f7d2c3ca41437
DreamStudioGroup <146511948+DreamStudioGroup@users.noreply.github.com>
2024-02-23 17:44:17 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,5 +1,5 @@
 function build_url(){
     var url = encodeURI(document.querySelector('#url').value);
-    url = "https://qiantigers.github.io/qq_fanghong/api/?url=" + url;
+    url = "https://dreamstudiogroup.github.io/api/?url=" + url;
     document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
 }
